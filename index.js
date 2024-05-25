f113a9d443bf90e0f487b2a30834cbbb834e3955
noriega146 <129232092+noriega146@users.noreply.github.com>
2024-05-24 21:28:40 -0400

    Update index.js
    //Part 2: Prime Time

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Part 1: Fizz Buzz
 // Counter 1-100
 for (let i = 1; i <= 100; i++) {
    //If a number is divisible by both 3 and 5, log “Fizz Buzz.”
@@ -9,8 +10,40 @@ for (let i = 1; i <= 100; i++) {
    //If a number is divisible by 5, log “Buzz.”
     } else if (i % 5 === 0) {
         console.log("Buzz");
-   //f a number is not divisible by either 3 or 5, log the number.
+   //if a number is not divisible by either 3 or 5, log the number.
     } else {
         console.log(i);
     }
 }
+
+//Part 2: Prime Time
+let n = 4; //Declare an arbitrary number, n.
+let foundprime = false;
+
+while (foundprime){
+    n++;
+    let isprime = true;
+
+    if (n <=1) {
+        isprime = false; //Prime number cannot be less than or equal to 1
+
+    } else if (n<=3) {
+        isprime = true //2 and 3 are prime numbers 
+    } else if (n%2 ===0 || n%3 ===0){
+        isprime = false; //if n is divisible by 2 or 3 prime = false
+    } else {
+        for (let i = 5; i*i <= n; i + 6){
+            if (n%i===0 || n % ( i+2) === 0){
+                isprime = false;
+                break;
+            }
+        }
+    }
+    if (isprime){
+        console.log(n);
+        foundprime = true; // As soon as you find the prime number, log that number and exit the loop.
+
+    }
+}
+
+
