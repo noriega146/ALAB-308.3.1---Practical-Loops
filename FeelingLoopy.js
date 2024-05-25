0a436e87fa366b991284d4d4eeeeb9241e29d68e
noriega146 <129232092+noriega146@users.noreply.github.com>
2024-05-24 22:15:51 -0400

    Update FeelingLoopy.js
    completed added notes

--- FeelingLoopy.js
+++ FeelingLoopy.js
@@ -11,6 +11,36 @@ let cell4='';
 let cellcounter = 1;
 
 //loop thru each character in csv
-for (let i = 0; i < csvString.length; i++) {
-    let char = csvString[i]; // Get the current character
+for (let i = 0; i < csvstrings.length; i++) {
+    let char = csvstrings[i]; // Get the current character
+
+    if (char === ',') {
+        // When a comma is encountered, move to the next cell
+        cellcounter++;
+    } else if (char === '\n') {
+        // When a /n is encountered, log the current row and reset cells and counter
+        console.log(cell1, cell2, cell3, cell4);
+        cell1 = '';
+        cell2 = '';
+        cell3 = '';
+        cell4 = '';
+        cellcounter = 1;
+    } else {
+        // Add the character to the appropriate cell based on the counter
+        if (cellcounter === 1) {
+            cell1 += char;
+        } else if (cellcounter === 2) {
+            cell2 += char;
+        } else if (cellcounter === 3) {
+            cell3 += char;
+        } else if (cellcounter === 4) {
+            cell4 += char;
+        }
+    }
+}
+
+// Log the last row if the CSV does not end with a newline
+if (cell1 || cell2 || cell3 || cell4) {
+    console.log(cell1, cell2, cell3, cell4);
+}
 
