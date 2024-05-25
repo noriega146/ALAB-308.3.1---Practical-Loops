b2c76c8cdd3bc2e5d9033bd76b884f1863917415
noriega146 <129232092+noriega146@users.noreply.github.com>
2024-05-24 21:02:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
-
-
-// Counter 1-100
-for (let i=1; i<101; i++) {console.log ("The number is "+i+"");}
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Fizz Buzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
